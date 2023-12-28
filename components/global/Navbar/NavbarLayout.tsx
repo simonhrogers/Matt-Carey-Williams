ff38d125b3bb73da87548906718bd485a5d34e50
simonhrogers <hello@simonrogers.info>
2023-12-28 19:39:18 +0000

    Navbar

--- components/global/Navbar/NavbarLayout.tsx
+++ components/global/Navbar/NavbarLayout.tsx
@@ -8,7 +8,9 @@ import type { MenuItem, SettingsPayload } from '@/types'
 import Lottie from "lottie-react"
 import Logo from 'assets/svg/Logo.svg'
 import LogoAnimation from "assets/svg/Logo.json"
-import { useRef } from 'react'
+import Menu from "assets/svg/Menu.svg"
+import { useEffect, useRef, useState } from 'react'
+import { useRouter } from 'next/navigation'
 
 interface NavbarProps {
   data: SettingsPayload
@@ -42,8 +44,48 @@ export default function Navbar(props: NavbarProps) {
 
   const logoRef = useRef()
 
+  const router = useRouter()
+  const asPath = router.asPath
+
+  const [isPhoneMenuActive, setIsPhoneMenuActive] = useState(false)
+  
+  const openPhoneMenu = () => {
+    setIsPhoneMenuActive(true)
+  }
+
+  const closePhoneMenu = () => {
+    setIsPhoneMenuActive(false)
+  }
+
+  const handleClick = (page) => {
+    if (page.slug === router.asPath) {
+      setIsPhoneMenuActive(false)
+    }
+  }
+
+  useEffect(() => {
+    closePhoneMenu()
+  }, [asPath])
+
+  // make header disappear on scroll down
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY
+    const visible = (currentScrollPos < 50 || prevScrollPos > currentScrollPos)
+    setPrevScrollPos(currentScrollPos)
+    setVisible(visible)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [prevScrollPos, visible, handleScroll])
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${(visible || isPhoneMenuActive) ? 'visible' : 'hidden'} ${isPhoneMenuActive ? 'active' : ''}`}>
       <div className='title-wrapper'>
         <div className="title">Matt Carey-Williams</div>
         <Lottie 
@@ -60,7 +102,15 @@ export default function Navbar(props: NavbarProps) {
           At Cork Street
         </div>
       </div>
-      <div className="menu-items">
+      <button
+        className={`phone-menu-button ${isPhoneMenuActive ? 'active' : ''}`}
+        onClick={isPhoneMenuActive ? closePhoneMenu : openPhoneMenu}
+        aria-label={isPhoneMenuActive ? 'Close Menu' : 'Open Menu'}
+      >
+        <Menu className={`svg ${isPhoneMenuActive ? 'active' : ''}`} />
+      </button>
+      {/* Laptop and up */}
+      <div className={`menu-items ${isPhoneMenuActive ? 'active' : ''}`}>
         {menuItems && menuItems.map((menuItem, key) => {
           const href = resolveHref(menuItem?._type, menuItem?.slug)
           if (!href) {
@@ -77,6 +127,24 @@ export default function Navbar(props: NavbarProps) {
           )
         })}
       </div>
+      {/* Tablet and down */}
+      {/* <div className="menu-items-small">
+        {menuItems && menuItems.map((menuItem, key) => {
+          const href = resolveHref(menuItem?._type, menuItem?.slug)
+          if (!href) {
+            return null
+          }
+          return (
+            <Link
+              key={key}
+              className={'menu-item'}
+              href={href}
+            >
+              {menuItem.title}
+            </Link>
+          )
+        })}
+      </div> */}
     </div>
   )
 }
