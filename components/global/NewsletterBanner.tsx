7f8d837184e0c878522b9b8c9c35758f82a731ec
Simon Rogers <hello@simonrogers.info>
2024-01-29 15:50:43 +0000

    Fixes

--- components/global/NewsletterBanner.tsx
+++ components/global/NewsletterBanner.tsx
@@ -1,26 +1,73 @@
 'use client'
 
-import { set } from "date-fns"
-import Link from "next/link"
-import { usePathname, useRouter } from "next/navigation"
-import { useState } from "react"
+import { usePathname } from "next/navigation"
+import { useRouter } from 'next/router'
+import { useEffect, useState } from "react"
 
 export function NewsletterBanner() {
 
-  const [open, setOpen] = useState(true)
+  const [closed, setClosed] = useState<string | null>('true')
+  const [keys, setKeys] = useState<string[]>([])
+  const [length, setLength] = useState<number>(0)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.sessionStorage) {
+      console.log('sessionStorage.getItem("closed")', sessionStorage.getItem('closed'))
+      let closed = sessionStorage.getItem('closed')
+      let keys: string[] = []
+      for (let i = 0; i < sessionStorage.length; i++) {
+        keys.push(sessionStorage.key(i)!)
+      }
+      setClosed(closed)
+      setKeys(keys)
+      setLength(sessionStorage.length)
+    }
+  }, [])
+
+  function handleSave(value) {
+    if (typeof window !== "undefined" && window.sessionStorage) {
+      sessionStorage.setItem("closed", value)
+      let closed = sessionStorage.getItem("closed")
+      let keys: string[] = [];
+      for (let i = 0; i < sessionStorage.length; i++) {
+        keys.push(sessionStorage.key(i)!);
+      }
+      setClosed(closed)
+      setKeys(keys);
+      setLength(sessionStorage.length);
+    }
+  }
+
+  // function handleRemove() {
+  //   if (typeof window !== 'undefined' && window.sessionStorage) {
+  //     sessionStorage.removeItem('closed')
+  //     setClosed("false");
+  //   }
+  // }
+
+  // function handleClear() {
+  //   if (typeof window !== 'undefined' && window.sessionStorage) {
+  //     sessionStorage.clear()
+  //     setClosed("false")
+  //   }
+  // }
 
   const pathname = usePathname()
+  const router = useRouter()
 
   const close = () => {
-    setOpen(false)
+    // setClosed(false)
+    // sessionStorage.setItem('newsletterBanner', 'false')
+    // console.log('sessionStorage.getItem("newsletterBanner")', sessionStorage.getItem('newsletterBanner'));
+    handleSave('true')
   }
 
-  const openModal = () => {
-    window.history.pushState({}, '', new URL(window.location.origin + `${pathname}?modal=newsletter`))
-    setOpen(false)
+  const closedModal = () => {
+    router.push(`${pathname}?modal=newsletter`, undefined, { shallow: true })
+    close()
   }
 
-  return open ? (
+  return (!closed || closed === 'false') ? (
     <div className="banner">
       <div className="banner-text">
         Subscribe to our newsletter to get our latest updates on upcoming exhibitions, events and more.
@@ -29,7 +76,7 @@ export function NewsletterBanner() {
         <button
           className="banner-option"
           onClick={(e) => {
-            openModal()
+            closedModal()
           }}
         >
           Subscribe
