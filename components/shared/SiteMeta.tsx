2a13c234e04049199dfc3b5108319938f83681b3
Simon Rogers <hello@simonrogers.info>
2024-02-02 16:50:34 +0000

    Favicon

--- components/shared/SiteMeta.tsx
+++ components/shared/SiteMeta.tsx
@@ -5,34 +5,31 @@ export default function SiteMeta() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/x-icon"
-        href="/favicon/favicon.ico"
+        href="/favicon.ico"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://cdn.sanity.io/" />
       <link rel="dns-prefetch" href="https://cdn.sanity.io/" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black" />
     </>
   )
 }
